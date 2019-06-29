92ccdeea7024d985eec8f8115e830cb7a30d8fee
neelkirit <neelkirit2@gmail.com>
2019-06-29 15:59:06 -0400

    Something that sends email
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+import email_util
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/send_email')
+def hello_world():
+    email_util.send()
+    return 'Mail Sent!'
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ email_util.py
@@ -0,0 +1,41 @@
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+
+
+def send():
+    port = 465  # For SSL
+    password = "Bakchod!@#$56"
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.domain.com", port, context=context) as server:
+        server.login("contact@gdgrochester.com", password)
+        sender_email = "contact@gdgrochester.com"
+        receiver_email = "neelkirit2@gmail.com"
+        message = """\
+            Subject: Hi there
+
+            This message is sent from Python."""
+
+        cont = '''\
+                   <html>
+                     <head></head>
+                     <body>
+                       <p>{0}</p>
+                     </body>
+                   </html>
+                   '''.format(message)
+
+        msgTo = ""
+        text_receivers = ['neelkirit2@gmail.com'
+                          ]
+        for person in text_receivers:
+            msgTo += "{0} ".format(person)
+
+        msg = MIMEText(cont, 'html')
+
+        msg['Subject'] = "Hello"
+        msg['From'] = "GDG Rochester <contact@gdgrochester.com>"
+        msg['To'] = msgTo
+        server.sendmail(sender_email, receiver_email, msg.as_string())
