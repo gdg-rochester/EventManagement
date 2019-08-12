510a071c257147a4933f8eb33c11e38c6772c1cc
neelkirit <neelkirit2@gmail.com>
2019-08-11 21:03:31 -0400

    Merging 2 POST Calls
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- devfest_util.py
+++ devfest_util.py
@@ -3,12 +3,16 @@ import requests
 
 def get_attendees():
     url = 'https://www.explara.com/api/e/attendee-list'
-    data = {'eventId': 'EBEHBIFF', 'fromRecord': 0, 'toRecord': 55}
+    data = {'eventId': 'EBEHBIFF', 'fromRecord': 1, 'toRecord': 50}
+    data_2 = {'eventId': 'EBEHBIFF', 'fromRecord': 51, 'toRecord': 100}
     headers = {
         'Authorization': 'Bearer 7a294cd04ca570bfb7c1dc763bf7a0c5f328d8b9',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'application/json'
     }
     response = requests.post(url, data=data, headers=headers)
+    response_2 = requests.post(url, data=data_2, headers=headers)
+    print(response.text)
+    # response = response + response_2
     response = response.json()
     return response
--- email_util.py
+++ email_util.py
@@ -13,30 +13,66 @@ def send(data):
     with smtplib.SMTP_SSL("smtp.domain.com", port, context=context) as server:
         server.login("contact@gdgrochester.com", password)
         sender_email = "contact@gdgrochester.com"
-        receiver_email = "neelkirit2@gmail.com"
-        message = """\
-            Subject: Hi there
+        receiver_email = "contact@gdgrochester.com"
+        # bcc = [
+        #     "lmontedoro@gmail.com",
+        #     "jnafus@frontiernet.net",
+        #     "davidryanbarnard@gmail.com",
+        #     "adarshreddy.r@gmail.com",
+        #     "abhilash@theagileschool.com",
+        #     "priyankarajan2590@gmail.com",
+        #     "fbfmcote@gmail.com",
+        #     "vrodriguez@itx.com",
+        #     "asmita231@gmail.com",
+        #     "ishiika.prasad@gmail.com",
+        #     "dfwarden@gmail.com",
+        #     "todd@toddbernhard.com",
+        #     "vganesh@paychex.com",
+        #     "kedargn94@gmail.com",
+        #     "ksjacques@paychex.com",
+        #     "virajchaudhari.2016@gmail.com",
+        #     "abrown@paychex.com",
+        #     "maxherman@gmail.com",
+        #     "drb4637@gmail.com",
+        #     "c.abhilash@gmail.com",
+        #     "jlad521@gmail.com",
+        #     "justin.z.tang94@gmail.com",
+        #     "phansen@paychex.com",
+        #     "Mhodesty@gmail.com",
+        #     "wangchoumi@gmail.com",
+        #     "pjwilbur99@gmail.com",
+        #     "zmyaro@zmyaro.com",
+        #     "Adityalandge1994@gmail.com",
+        #     "Darwyncook@gmail.com",
+        #     "mjohnson2898@gmail.com",
+        #     "omkarvaidya13@gmail.com",
+        #     "georgecolgrove@gmail.com",
+        #     "priyapriya2590@gmail.com",
+        #     "ayushsoni638@gmail.com",
+        #     "ikunal03@gmail.com",
+        #     "ayushipatra369@gmail.com",
+        #     "ashwini.jaitapkar9@gmail.com",
+        #     "junaidazharshaikh@gmail.com",
+        #     "ssuloglu@gmail.com",
+        #     "ajinkyarode3@gmail.com",
+        #     "pwagh@buffalo.edu",
+        #     "sanyuktakate@gmail.com",
+        #     "chandak.kishor14@gmail.com"
+        #     ]
 
-            This message is sent from Python."""
+        bcc = ["neelkirit2@gmail.com", "narkhedesarang@gmail.com"]
 
-        cont = '''\
-                   <html>
-                     <head></head>
-                     <body>
-                       <p>{0}</p>
-                     </body>
-                   </html>
-                   '''.format(data['message'])
-
+        cont = '''{0}'''.format(data["message"])
         msgTo = ""
-        text_receivers = ['neelkirit2@gmail.com'
+        text_receivers = ['contact@gdgrochester.com'
                           ]
         for person in text_receivers:
             msgTo += "{0} ".format(person)
 
         msg = MIMEText(cont, 'html')
 
-        msg['Subject'] = "Hello"
+        msg['Subject'] = '''{0}'''.format(data["subject"])
         msg['From'] = "GDG Rochester <contact@gdgrochester.com>"
         msg['To'] = msgTo
+        receiver_email = [receiver_email] + bcc
         server.sendmail(sender_email, receiver_email, msg.as_string())
