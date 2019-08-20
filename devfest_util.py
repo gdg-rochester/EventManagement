d0dc6acf88efd76dcd9024fe31fdd6a06e68ecaa
Neel Kirit <neelkirit2@gmail.com>
2019-08-20 11:55:58 -0400

    Refactored Code

--- devfest_util.py
+++ devfest_util.py
@@ -1,34 +1,27 @@
 import requests
 
 
-def get_attendees():
+def get_attendees(start_index, end_index):
     url = 'https://www.explara.com/api/e/attendee-list'
-    data = {'eventId': 'EBEHBIFF', 'fromRecord': 0, 'toRecord': 50}
-    # data = {'eventId': 'EBEHBIFF', 'fromRecord': 50, 'toRecord': 100}
+    data = {'eventId': 'EBEHBIFF', 'fromRecord': start_index, 'toRecord': end_index}
     headers = {
         'Authorization': 'Bearer 7a294cd04ca570bfb7c1dc763bf7a0c5f328d8b9',
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept': 'application/json'
     }
     response = requests.post(url, data=data, headers=headers)
-    # response_2 = requests.post(url, data=data_2, headers=headers)
-    print(response.text)
-    # response = response + response_2
     response = response.json()
     return response
 
 
-def get_attendees_2():
-    url = 'https://www.explara.com/api/e/attendee-list'
-    data = {'eventId': 'EBEHBIFF', 'fromRecord': 50, 'toRecord': 100}
-    headers = {
-        'Authorization': 'Bearer 7a294cd04ca570bfb7c1dc763bf7a0c5f328d8b9',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json'
-    }
-    response = requests.post(url, data=data, headers=headers)
-    # response_2 = requests.post(url, data=data_2, headers=headers)
-    print(response.text)
-    # response = response + response_2
-    response = response.json()
-    return response
+# def get_attendees_2():
+#     url = 'https://www.explara.com/api/e/attendee-list'
+#     data = {'eventId': 'EBEHBIFF', 'fromRecord': 50, 'toRecord': 100}
+#     headers = {
+#         'Authorization': 'Bearer 7a294cd04ca570bfb7c1dc763bf7a0c5f328d8b9',
+#         'Content-Type': 'application/x-www-form-urlencoded',
+#         'Accept': 'application/json'
+#     }
+#     response = requests.post(url, data=data, headers=headers)
+#     response = response.json()
+#     return response
