917d3aa03530351655538b8a324bed56a24590e1
Neel Kirit <neelkirit2@gmail.com>
2019-08-22 23:21:04 -0400

    Refactored Code

--- devfest_util.py
+++ devfest_util.py
@@ -12,16 +12,3 @@ def get_attendees(start_index, end_index):
     response = requests.post(url, data=data, headers=headers)
     response = response.json()
     return response
-
-
-# def get_attendees_2():
-#     url = 'https://www.explara.com/api/e/attendee-list'
-#     data = {'eventId': 'EBEHBIFF', 'fromRecord': 50, 'toRecord': 100}
-#     headers = {
-#         'Authorization': 'Bearer 7a294cd04ca570bfb7c1dc763bf7a0c5f328d8b9',
-#         'Content-Type': 'application/x-www-form-urlencoded',
-#         'Accept': 'application/json'
-#     }
-#     response = requests.post(url, data=data, headers=headers)
-#     response = response.json()
-#     return response
