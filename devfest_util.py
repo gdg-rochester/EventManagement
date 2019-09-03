061805bcd6acc2bb6fbdb2e30040342605564356
neelkirit <neelkirit2@gmail.com>
2019-09-03 19:37:06 -0400

    Removed Explara API
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- devfest_util.py
+++ devfest_util.py
@@ -1,13 +1,7 @@
-import requests
+import pandas as pd
+
 
 def get_attendees(start_index, end_index):
-    url = 'https://www.explara.com/api/e/attendee-list'
-    data = {'eventId': 'EBEHBIFF', 'fromRecord': start_index, 'toRecord': end_index}
-    headers = {
-        'Authorization': 'Bearer 7a294cd04ca570bfb7c1dc763bf7a0c5f328d8b9',
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Accept': 'application/json'
-    }
-    response = requests.post(url, data=data, headers=headers)
-    response = response.json()
-    return response
+    file_name = "attendee_list.csv"
+    pd.read_csv(file_name)
+    return None
