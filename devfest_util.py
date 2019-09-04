b064eb2f877f2c2761bed45419128bc0aabbdede
neelkirit <neelkirit2@gmail.com>
2019-09-04 19:35:31 -0400

    Added CSV read logic
    Signed-off-by: neelkirit <neelkirit2@gmail.com>

--- devfest_util.py
+++ devfest_util.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
 
-def get_attendees(start_index, end_index):
-    file_name = "attendee_list.csv"
-    pd.read_csv(file_name)
+def get_attendees():
+    csv_file = pd.DataFrame(pd.read_csv("attendee_list.csv", sep=",", header=0, index_col=False))
+    csv_file.to_json("file.json", orient="records", date_format="epoch", double_precision=10,
+                     force_ascii=True, date_unit="ms", default_handler=None)
+    print(csv_file)
     return None
