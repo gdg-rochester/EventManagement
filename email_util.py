b4166dacc9007c58d491bb2f205addb5e223154b
Neel Kirit <neelkirit2@gmail.com>
2019-09-12 22:10:04 -0400

    Removed emails

--- email_util.py
+++ email_util.py
@@ -14,51 +14,7 @@ def send(data):
         server.login("contact@gdgrochester.com", password)
         sender_email = "contact@gdgrochester.com"
         receiver_email = "contact@gdgrochester.com"
-        # bcc = [
-        #     "lmontedoro@gmail.com",
-        #     "jnafus@frontiernet.net",
-        #     "davidryanbarnard@gmail.com",
-        #     "adarshreddy.r@gmail.com",
-        #     "abhilash@theagileschool.com",
-        #     "priyankarajan2590@gmail.com",
-        #     "fbfmcote@gmail.com",
-        #     "vrodriguez@itx.com",
-        #     "asmita231@gmail.com",
-        #     "ishiika.prasad@gmail.com",
-        #     "dfwarden@gmail.com",
-        #     "todd@toddbernhard.com",
-        #     "vganesh@paychex.com",
-        #     "kedargn94@gmail.com",
-        #     "ksjacques@paychex.com",
-        #     "virajchaudhari.2016@gmail.com",
-        #     "abrown@paychex.com",
-        #     "maxherman@gmail.com",
-        #     "drb4637@gmail.com",
-        #     "c.abhilash@gmail.com",
-        #     "jlad521@gmail.com",
-        #     "justin.z.tang94@gmail.com",
-        #     "phansen@paychex.com",
-        #     "Mhodesty@gmail.com",
-        #     "wangchoumi@gmail.com",
-        #     "pjwilbur99@gmail.com",
-        #     "zmyaro@zmyaro.com",
-        #     "Adityalandge1994@gmail.com",
-        #     "Darwyncook@gmail.com",
-        #     "mjohnson2898@gmail.com",
-        #     "omkarvaidya13@gmail.com",
-        #     "georgecolgrove@gmail.com",
-        #     "priyapriya2590@gmail.com",
-        #     "ayushsoni638@gmail.com",
-        #     "ikunal03@gmail.com",
-        #     "ayushipatra369@gmail.com",
-        #     "ashwini.jaitapkar9@gmail.com",
-        #     "junaidazharshaikh@gmail.com",
-        #     "ssuloglu@gmail.com",
-        #     "ajinkyarode3@gmail.com",
-        #     "pwagh@buffalo.edu",
-        #     "sanyuktakate@gmail.com",
-        #     "chandak.kishor14@gmail.com"
-        #     ]
+       
 
         bcc = ["neelkirit2@gmail.com", "narkhedesarang@gmail.com"]
 
